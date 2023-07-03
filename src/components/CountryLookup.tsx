ef4957768750ee82e07fafff8f064a3e1d10e997
Oscar <oscar.galindo1@yahoo.com>
2023-07-03 09:04:04 -0600

    added footer component

---
+++ src/components/CountryLookup.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+export default function CountryLookup() {
+	const [country, setCountry] = useState('United States');
+	const getCountry = async () => {
+		const res = await fetch(
+			`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
+		);
+		const data = await res.json();
+		setCountry(data.country);
+	};
+	useEffect(() => {
+		getCountry();
+	}, []);
+
+	return <div>{country}</div>;
+}
