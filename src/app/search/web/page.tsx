75a22c849e5d6d59a5965ec763a1e2f7a5937511
Oscar <oscar.galindo1@yahoo.com>
2023-07-03 20:05:48 -0600

    added error handling

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,9 +1,13 @@
 import { Item } from '@/app/interfaces/SearchResult';
+import Link from 'next/link';
 
 async function search(term: string) {
 	const res = await fetch(
 		`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CTX_KEY}&q=${term}`
 	);
+	if (!res.ok) {
+		throw new Error('Something went wrong');
+	}
 	const { items } = await res.json();
 	console.log(items);
 	return items;
@@ -15,6 +19,18 @@ export default async function WebSearchPage({
 	searchParams: { searchTerm: string };
 }) {
 	const results = await search(searchParams.searchTerm);
+
+	if (!results) {
+		return (
+			<div className="flex flex-col justify-center items-center pt-10">
+				<h1 className="text-3xl mb-4">No results found</h1>
+				<p className="text-lg">
+					Try searching for something else or go back to the <Link href="/" className="text-blue-500 text-center">Homepage.</Link>
+				</p>
+			</div>
+		);
+	}
+
 	return (
 		<>{results && results.map((result: Item) => <h1>{result.title}</h1>)}</>
 	);
--- src/components/CountryLookup.tsx
+++ src/components/CountryLookup.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 
 export default function CountryLookup() {
-	const [country, setCountry] = useState('United States');
+	const [country, setCountry] = useState('');
 	const getCountry = async () => {
 		const res = await fetch(
 			`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
